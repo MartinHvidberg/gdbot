cbbb94ed1ee2a154deeb48be32811f965c981df1
Hanne Petersen <halpe@gst.dk>
2014-01-27 16:38:09 +0100

    v. 0.2; revising the gdbot syntax

--- gdbot.py
+++ gdbot.py
@@ -6,20 +6,18 @@
     Author: Martin Hvidberg <mahvi@gst.dk>
     Author: Hanne L. Petersen <halpe@gst.dk>
     
-    Ver. 0.1
+    Ver. 0.2
     
     To do:
     
-        Look for XXX in the code 
+        - Work with NIS, not just with .gdb
         
-        Allow the rule reader to understand S-57 (6-letter) codes, e.g. PortsAndServicesP FCsubtype 15 is actually called CRANES
-            : 3 : Cranes can no longer be CATCRN = 1. Change to unknown : PortsAndServicesP : 15 : CATCRN = 1 : : FIX : -32767 :
-            : 3 : Cranes can no longer be CATCRN = 1. Change to unknown : PortsAndServicesP : CRANES : CATCRN = 1 : : FIX : -32767 :
-    
+        - Figure out why we can't have double quotes in condition input. Fix it, convert them, or detect and issue a warning
+        
 """
 
-###import arcpy
-#import os
+import arcpy
+import os, re
 from string import upper, replace
 from datetime import datetime # for datetime.now()
 import S57names
@@ -46,6 +44,11 @@ lstNISlayers = ['AidsToNavigationP',
                 'TracksAndRoutesA', 'TracksAndRoutesL', 'TracksAndRoutesP', 
                 'UserDefinedFeaturesA', 'UserDefinedFeaturesL', 'UserDefinedFeaturesP']
 
+floatPattern = re.compile("^[0-9.,]+$")
+def isfloat(string):
+    """ Check if string looks like a float (or int) number. """
+    return re.search(floatPattern, string)
+
 def log(string):
     """Append message to the log"""
     global lst_log
@@ -62,84 +65,105 @@ def writeLogToFile(filename):
             thefile.write("%s\n" % logline)
 
 class Rule:
-    def __init__(self, theid, title, fc, fcsubtype, mainfield, errval, othercond, fixorlog, fixvalue):
+    def __init__(self, theid, title, mode, fc, fcsubtype, condition, fixorlog, fixvalue):
         # The ID
         self.id = theid
         # Title
         self.title = title
-        # FC
+        # Mode
+        self.mode = upper(mode)
+        if self.mode != "SQL":
+            log("Warning, rule {}: Only SQL mode is supported, unknown mode {}.".format(self.id, mode))
+        # FC - self.fclist is a list, possibly with only one element
         if(fc=="*"):
             self.fclist = lstNISlayers
             if(fcsubtype != "*"):
                 log("Warning, rule {}: Feature class is *, but feature class subtype is {}.".format(self.id, fcsubtype))
         else:
             self.fclist = fc.split(",") # split will return a list, even if there are no commas
-        # FCsubtype
+        # FCsubtype - self.fcsubtype is a comma-separated string, NOT a list
         fcsubtype = fcsubtype.strip()
         if fcsubtype == "*" or fcsubtype == "":
             self.fcsubtype = "*"
+        elif(re.search("[A-Za-z]", fcsubtype) and len(self.fclist)>1): # can't handle s57-fcs with multiple fc
+            log("Error, rule {}: Can't handle fc subtype abbreviations ({}) for multiple feature classes ({}).".format(self.id, fcsubtype, fc))
+            self.id = -1
+            return None
         else:
-            if isinstance(fcsubtype,int):
-                self.fcsubtype = int(fcsubtype)
-            else: # If it's not an integer, it may be an S-57 '6-letter-code'
-                fcs_value = S57names.S57ABB2FCS(fcsubtype)
-                if fcs_value > 0:
-                    self.fcsubtype = fcs_value
-                else:
-                    log("Warning, rule {}: Can't interpret fcsubtype: {}.".format(self.id, fcsubtype))
-                    self.fcsubtype = -999
-        # Main field to check
-        self.field = mainfield
-        # Error value for main field
-        self.errval = replace(errval, "!=", "<>")
-        if(self.errval[0] != "=" and upper(self.errval[0:2]) != "IS" and self.errval[0:2] != "<>"):
-            log("Warning, rule {}: no = or IS operator in error condition: {} ?".format(str(self.id), errval[0:2]))
-        # Other condition
-        self.othercond = replace(othercond, "!=", "<>")
-        # Fix or Log
+            if("," in fcsubtype):
+                # split the list by comma, convert each to int if needed, and glue with commas again
+                self.fcsubtype = ",".join([GetFCSids(fcs, self.fclist[0]) for fcs in fcsubtype.split(",")])
+            else:
+                self.fcsubtype = GetFCSids(fcsubtype, self.fclist[0])
+        # Condition
+        self.condition = replace(condition, "!=", "<>")
+        # Fix or Log - self.dofix is a bool
         self.dofix = (fixorlog=="FIX")
-        # Fix value (new value for main field)
-        # This is ugly: we're stripping quote marks off fixvalue, even though it's a string.
-        # They're required in the test value, but not allowed in the fix value,
-        # so we'll allow the user to enter them in both places, and remove them here.
-        #=======================================================================
-        if(fixvalue and fixvalue[0] == fixvalue[-1] and (fixvalue[0]=="'" or fixvalue[0]=='"')):
-            self.fixvalue = fixvalue[1:-1]
-        else:
-            self.fixvalue = fixvalue
-        #self.fixvalue = fixvalue.strip("\"\'") # This is shorter - but less accurate, and it's already iffy...
-        #if upper(self.fixvalue) == "UNKNOWN":
-        #    self.fixvalue = -32767 # is this a good idea? other values to accept?
-        #if upper(self.fixvalue) == "NONE":
-        #    self.fixvalue = NULL
-        #=======================================================================        
-        # TODO: recognize a fixvalue which is another column name
-        if(self.dofix and not self.fixvalue):
-            self.dofix = False
-            log("Warning, rule {}: FIX with no repair value; treating as LOG.".format(str(self.id)))
-             # if the user didn't supply a fix value, this is more helpful than throwing an error
-        if(self.fixvalue and not self.dofix):
-            log("Warning, rule {}: FIX is not set, but repair value is non-empty.".format(str(self.id)))
+        self.fixLst = []
+        # Fix value (new value for main field) - self.fixLst is a list of pairs (lists)
+        if self.dofix:
+            if fixvalue: # there's probably a cleaner way of doing this looping and cleaning...
+                # TODO: recognize a fixvalue which is another column name
+                self.fixLst = [fixpair.split("=") for fixpair in fixvalue.split(",")] # split on , and =
+                self.fixLst = [[val.strip() for val in fixpair] for fixpair in self.fixLst] # strip whitespace
+                for fix in self.fixLst:
+#                     fix = [val.strip() for val in fix] # strip whitespace
+                    fix[1] = CleanUpFixString(fix[1]) # ugly quote mark removal, recognising None, NULL, UNKNOWN and typecasting int/float
+                print self.fixLst
+            else:
+                self.dofix = False
+                log("Warning, rule {}: FIX with no repair values; treating as LOG.".format(str(self.id)))
+                 # if the user didn't supply a fix value, this is more helpful than throwing an error
+        elif fixvalue:
+                log("Warning, rule {}: FIX is not set, but repair value is non-empty.".format(str(self.id)))
     def GetWhereString(self):
-        """Return a string with the WHERE clause for the rule, includes: field, errvalue, fcsubtype, and othercond."""
-        where = self.field + ' ' + self.errval
+        """Return a string with the WHERE clause for the rule, includes: condition and fcsubtype."""
+        where = self.condition
         if(self.fcsubtype != "*"):
             where = where + " AND FCSubtype"
             if "," in self.fcsubtype:
                 where = where + " IN (" + self.fcsubtype + ")"
             else:
                 where = where + " = " + self.fcsubtype
-        if(self.othercond):
-            where = where + " AND " + self.othercond
         return where
     def __repr__(self):
-        fixstring = ""
-        if(self.dofix):
-            fixstring = "{} = {}".format(str(self.field), str(self.fixvalue))
-        return "({}, {}, {}, ({}), \"{}\", {}:\"{}\")\n".format(\
+        return "({}; {}; {}; {}; \"{}\"; {}:{})\n".format(\
         str(self.id), str(self.title), str(self.fclist), str(self.fcsubtype),
-            self.GetWhereString(), str(self.dofix), fixstring)
+            self.GetWhereString(), str(self.dofix), str(self.fixLst))
 # end class Rule
+
+def GetFCSids(fcsubtype, fc):
+    """Return the number (as string) for the fc subtype."""
+    fcsubtype = fcsubtype.strip()
+    if fcsubtype.isdigit():
+        return fcsubtype
+    else: # If it's not an integer, it may be an S-57 '6-letter-code'
+        fcs_value = S57names.S57ABBFC2FCSNumber(fcsubtype, fc)
+        if fcs_value > 0:
+            return fcs_value
+        fcs_value = S57names.S57ABBFC2FCSNumber(fcsubtype, upper(fc)) # if we didn't find it, convert to uppercase and try again
+        if fcs_value > 0:
+            return fcs_value
+        log("Warning: Can't interpret fcsubtype: {}.".format(fcsubtype))
+        return -1
+
+def CleanUpFixString(fixvalue):
+    """ If string starts and ends with matching quote marks, remove these; also convert None, NULL (and UNKNOWN), and convert to int/double. """
+    # This is ugly: we're stripping quote marks off the fixvalue, even when it's a string.
+    # They're required in the test value, but not allowed in the fix value,
+    # so we'll allow the user to enter them in both places, and remove them here.
+    if(fixvalue and fixvalue[0] == fixvalue[-1] and (fixvalue[0]=="'" or fixvalue[0]=='"')):
+        fixvalue = fixvalue[1:-1]
+    #if fixvalue.upper() == "UNKNOWN":
+    #    return -32767 # TODO: is this a good idea? other values to accept?
+    if fixvalue.upper() == "NULL" or fixvalue.upper() == "NONE":
+        return None # make sure to return, the following lines will choke on a None
+    if fixvalue.isdigit():
+        return int(fixvalue)
+    if isfloat(fixvalue): # this will also match on int, so check that first
+        return float(fixvalue.replace(",", ".", 1)) # accept either , or . as decimal separator
+    return fixvalue
+
 
 def ConnectToDB(db):
     arcpy.env.workspace = db
@@ -180,53 +204,64 @@ def ReadRules(path):
         if("#" in line):
             line = line.split("#")[0].strip()
         items = line.split(":") 
-        if len(items)!=11:
+        if len(items)!=10:
            log("Warning: Line does not contain the correct number of elements... \n\t"+line.strip()+"\n\t"+repr(items))
         # forget about number 0, since it's always an empty string (nothing in front of the first ':')
-        # number 10 is just comments
+        # number 9 is just comments
         ruleid = items[1].strip()
         title = items[2].strip()
-        featureclass = items[3].strip()
-        fcsubtype = items[4].strip()
-        field = items[5].strip()
-        errorvalue = items[6].strip()
-        sec_cond = items[7].strip()
-        fixorlog = items[8].strip()
-        fixvalue = items[9].strip()
-        r = Rule(ruleid, title, featureclass, fcsubtype, field, errorvalue, sec_cond, fixorlog, fixvalue)
-        lst_rules.append(r)
+        mode = items[3].strip()
+        featureclass = items[4].strip()
+        fcsubtype = items[5].strip()
+        condition = items[6].strip()
+        fixorlog = items[7].strip()
+        fixvalue = items[8].strip()
+        r = Rule(ruleid, title, mode, featureclass, fcsubtype, condition, fixorlog, fixvalue)
+        if r.id != -1:
+            lst_rules.append(r)
     f.close()
     print "Done reading rules."
     return lst_rules
 
 def CheckTables(dataset, rules):
     """Check the tables of the given dataset, according to the rules"""
+    # If you get: "RuntimeError: An expected Field was not found or could not be retrieved properly."
+    # don't use double quotes in condition input, use single quotes!!!
     for rule in rules:
         if(rule.dofix): # FIX
-            #print "UPDATE " + str(rule.fclist) + " SET " + rule.field + " = " + rule.fixvalue + " WHERE " + rule.GetWhereString()
+            # v. 0.1: print "UPDATE " + str(rule.fclist) + " SET " + rule.field + " = " + rule.fixvalue + " WHERE " + rule.GetWhereString()
             for fc in rule.fclist:
                 count = 0
-                fields = ["PLTS_COMP_SCALE", "LNAM", rule.field] # watch the numbering, update below!
-                # TODO: if fixvalue is column name, do we need to include it here?
-                with arcpy.da.UpdateCursor(dataset+"Nautical/"+fc, fields, rule.GetWhereString()) as uc:
+                fields = ["OBJECTID", "LNAM"] # fields to get from the db, update log writing below if this changes
+                defaultFieldsNum = len(fields)
+                for correction in rule.fixLst: # add the columns that we're going to fix
+                    fields.append(correction[0])
+                with arcpy.da.UpdateCursor(dataset+"Nautical/"+fc, fields, where_clause=rule.GetWhereString()) as uc:
                     for row in uc:
                         count += 1
-                        log("Rule {} ({}): updating {} = {} (was {}) for LNAM={}"
-                            .format(rule.id, rule.title, rule.field, rule.fixvalue, row[2], row[1]))
-                        row[2] = rule.fixvalue
-                        uc.updateRow(row)
+                        logStr = "Rule {} ({}): updating " .format(rule.id, rule.title)
+                        i = defaultFieldsNum
+                        for i in range(len(rule.fixLst)): # do each of the fixes
+                            logStr += "{} = {} (was {}), ".format(rule.fixLst[i][0], rule.fixLst[i][1], row[defaultFieldsNum+i])
+                            fixVal = rule.fixLst[i][1] # set the update value
+                            row[defaultFieldsNum+i] = fixVal
+                            i += 1
+                        logStr = logStr[0:-2] + " for OBJECTID = {} in {}".format(row[0], fc)
+                        log(logStr)
+                        uc.updateRow(row) # do the actual update
                         pass
                 log("Total {} fix hits for rule {}".format(count, rule.id))
         else: # LOG
-            #print "SELECT FROM " + str(rule.fclist) + " WHERE " + rule.GetWhereString()
+            # v. 0.1: print "SELECT FROM " + str(rule.fclist) + " WHERE " + rule.GetWhereString()
             for fc in rule.fclist:
                 count = 0
-                fields = ["PLTS_COMP_SCALE", "LNAM", rule.field]
+                fields = ["OBJECTID", "LNAM"] # fields to get from the db, update log writing below if this changes
                 with arcpy.da.SearchCursor(dataset+"Nautical/"+fc, fields, where_clause=rule.GetWhereString()) as sc:
                     for row in sc:
                         count += 1
-                        log("Found rule {} ({}) violation with {}={} for LNAM={} in {}"
-                            .format(rule.id, rule.title, rule.field, row[2], row[1], fc))
+                        log("Found rule {} ({}) violation for OBJECTID={} in {}"
+                            .format(rule.id, rule.title, row[0], fc))
+                        # TODO: Nice to have: include the current value of the field(s) - can we parse the requested field names out of the condition string
                 log("Total {} search hits for rule {}".format(count, rule.id))
     log("Done checking tables.")
 
@@ -244,30 +279,33 @@ def RunGdbTests():
     for rule in lst_rules:
         print " Rule: "+rule.__repr__().strip()
     print "Finished read tests."
-    #return
+    return
     
     lst_rules = []
     
     print "Starting check tests..."
-    dataset = "./clip_67_67_5_til_1411.gdb/"
+    #dataset = "./clip_67_67_5_til_1411.gdb/"
     #dataset = "./1462hAndheld.gdb/"
-    dataset_copy = "./testdata.gdb/"
+    datazip = "../test.gdb.zip"
+    dataset = ".\\"+datazip[3:-4]+"\\"
     try:
-        #print "deleting..."
-        #arcpy.Delete_management(dataset_copy)
-        #os.system("del /q %s" % (dataset_copy))
-        #print "copying:"
-        #arcpy.Copy_management(dataset, dataset_copy)
-        #os.system("xcopy /s %s %s" % (dataset, dataset_copy))
+        print "deleting test gdb..."
+#         os.system("del /q %s" % (dataset))
+        print "extracting test gdb..."
+#         os.system("unzip -q %s" % (datazip))
+        # unzipping will produce an error:
+        # test.gdb/:  ucsize 163840 <> csize 0 for STORED entry ; continuing with "compressed" size value
+        # but seems to work fine anyway
         pass
-    except ExecuteError as e:
-        print "deleting failed: " + str(e)
+    except BaseException as e:
+        print "system command (delete/extract/...) failed: " + str(e)
     lst_rules = ReadRules(checkrules)
     print lst_rules
-    CheckTables(dataset_copy, lst_rules)
+    CheckTables(dataset, lst_rules)
     print "Finished check tests."
     
     # TODO: compare the log file output to the expected output
+    # 'C:\Program Files (x86)\WinMerge\WinMergeU.exe' ...
     # TODO: compare the resulting gdb to the expected result
     #writeLogToFile('log.txt')
 
@@ -283,7 +321,7 @@ def main(db, rulefile, logfile):
     # Create a version, if applicable, and change to it
     try:
         dbVersion = arcpy.CreateVersion_management (conDB,"NIS.DEFAULT","kill999","PROTECTED")
-        print "Check...",dbVersion
+        print "Check...", dbVersion
     except arcpy.ExecuteError as e:
         print repr(e)#"I/O error({0}): {1}".format(e.errno, e.strerror)
     
@@ -293,6 +331,7 @@ def main(db, rulefile, logfile):
     CheckTables(conDB, lstRules)
     
     # Reconcile and Post the version, if allowed by the run mode
+    # arcpy.ReconcileVersions_management
     
     # Stop the edit session
     
@@ -307,10 +346,12 @@ def main(db, rulefile, logfile):
     
 if __name__ == "__main__":
     RunGdbTests()
+    db = "M:\HAL\TeamNIS\Tools\SDE_connections\Yellow_Test\yellow_nis@NIS_EDITOR.sde"
+    #db = "M:\HAL\TeamNIS\Tools\SDE_connections\green2_nis@nis_editor.sde"
     #db = "Database Connections\green2_nis@nis_editor.sde"
     #db = "C:\Martin\Work_Eclipse\BuildGreen\data\input.gdb"
     db = "./input.gdb"
     #main(db, rules, "log.txt")
 
 else:
-    print "Non recognized caller: "+__name__
+    print "Non-recognized caller: "+__name__
