590fd7ab49bccf41d827d47baa6555694f533992
Martin Hvidberg <Martin@Hvidberg.net>
2015-03-06 19:09:23 +0100

    Update gdbot.py
    uploading from production environment '150306/mahvi

--- gdbot.py
+++ gdbot.py
@@ -6,10 +6,18 @@
     Author: Martin Hvidberg <mahvi@gst.dk>
     Author: Hanne L. Petersen <halpe@gst.dk>
     
-    # Ver. 0.2: Fundamental changes to rule input format
-    # Ver. 0.3: allowing FIX action to include a list of fields to report
+    SYNTAX:  gdbot.py rulefile logfile database_connection
+        e.g. gdbot.py rules\nulls.gdbot logs\nulls.log nis_editor@green3.sde
+    
+    # Ver. 0.1: ; gdbot syntax version 1.0 (e.g. no Mode field, and different Condition syntax from 1.1)
+    # Ver. 0.2: Fundamental changes to rule input format ; gdbot syntax version 1.1
+    # Ver. 0.3: allowing FIX action to include a list of fields to report ; gdbot syntax version 1.2
+    # Ver. 0.4: introducing LOVE (List Of Valid Elements) mode
     
     To do:
+        - Allow checks for other fds than Nautical (separate mode?)
+        
+        - Allow to check in fc's  with wildcard matching names (e.g. like '%anno%')
     
         - Find out if we want to use versioning - changes still arrive in products without versions
         
@@ -22,11 +30,12 @@
         - Consider a MAIL action option, that will (log and) email the last editor
 
 """
-import os
+import os, sys
 from datetime import datetime # for datetime.now()
-import arcpy
 
+sys.path.append(r'\\hnas1evs2.kms.adroot.dk\vol4\KMS\o-kms\_TeamNIS_Produktion\BatchScripts\common') # TODO: use os.getcwd()...
 import utils
+
 import rule_parser
 import data_checker # comment this out to avoid importing arcpy
 
@@ -80,55 +89,90 @@ def RunGdbTests():
     #writeLogToFile('log.txt')
 
 
-def main(db, rulefile, logfile):
+def main(db, rulefile, logfile, mails):
+    
+    print "  Output goes to: "+logfile
+    utils.log("  Current execute path: "+os.getcwd())
+    print "Rulefile: "+rules
+    print "Database connection: "+db
+    if len(mails) == 0:
+        print "No email."
+    else:
+        print "Email will be sent to: "+str(mails)
     
     timStart = datetime.now()
+    
     # Read the .gdbot file and build the list of bot-rules
     lstRules = rule_parser.ReadRules(rulefile)
     #print lstRules
     
     if isinstance(lstRules, int): # if ReadRules returned a number, it's an error code...
-        print "ReadRules returned an error..."
+        utils.log("ReadRules returned an error...")
         return lstRules
 
     print "Number of rules: "+str(len(lstRules))
+    utils.log("  Checking {}, {} rules".format(rulefile, len(lstRules)))
     
     data_checker.CheckData(db, lstRules)
 
+    timEnd = datetime.now()
+    durRun = timEnd - timStart
+    utils.log("   Total " + __file__ + " duration (h:mm:ss.dd): " + str(durRun)[:-3])
+    
     # Finish off logfiles, etc. and clean up nicely...
-    utils.writeLogToFile('log.txt')
+    if len(logfile) > 0:
+        utils.writeLogToFile(logfile)
+    if len(mails) > 0:
+        utils.sendLogToEmail(mails, 'gdbot run, {}'.format(rulefile.split('\\')[-1]), "Found")
     
     timEnd = datetime.now()
     durRun = timEnd - timStart
-    print "\n  Total " + __file__ + " duration (h:mm:ss.d): " + str(durRun)[:-3]
+    print "\n   Total " + __file__ + " duration (h:mm:ss.dd): " + str(durRun)[:-3]
 
     return 0
-    
+
+def printHelp():
+    print "python gdbot.py rulefile logfile connectionfile email"
     
 if __name__ == "__main__":
 
     #RunGdbTests()
 
-    #rules = "ruletest.gdbot"
-    #rules = "test.gdbot"
-    #rules = "M:/HAL/TeamNIS/Tools/PythonStuff/gdbot/rule_generators/nulls.gdbot"
-    rules = "M:/HAL/TeamNIS/Tools/PythonStuff/gdbot/gst_rules/S5758.gdbot"
-    
-    db = "M:/HAL/TeamNIS/Tools/SDE_connections/Yellow3/nis_editor@yellow3.sde"
-    #db = "M:/HAL/TeamNIS/Tools/SDE_connections/nis_editor@green3.sde"
-    #db = "C:\Martin\Work_Eclipse\BuildGreen\data\input.gdb"
-#     db = "./test.gdb"
+    #rules = r"\\hnas1evs2.kms.adroot.dk\vol4\KMS\o-kms\_TeamNIS_Produktion\BatchScripts\gdbot\rules\misc-gst-rules.gdbot"
+    rules = r"\\hnas1evs2.kms.adroot.dk\vol4\KMS\o-kms\_TeamNIS_Produktion\BatchScripts\gdbot\rules\test.gdbot"
+    #rules = r"\\hnas1evs2.kms.adroot.dk\vol4\KMS\o-kms\_TeamNIS_Produktion\BatchScripts\gdbot\rules\nulls-log.gdbot"
 
-    logfile = "log.txt"
+#    logfile = "log.txt"
+    logfile = r"\\hnas1evs2.kms.adroot.dk\vol4\KMS\o-kms\_TeamNIS_Produktion\BatchScripts\gdbot\log.txt"
+
+    db = r"\\hnas1evs2.kms.adroot.dk\vol4\KMS\o-kms\_TeamNIS_Produktion\BatchScripts\sde_filer\nis_editor@green3.sde"
+
+    mails = []#['halpe@gst.dk']
+    
+    if (len(sys.argv)>1):
+        rules = sys.argv[1]
+    #else:
+    #    printHelp()
+    #    # exit or just run with default (test.gdbot)?
+    #    exit # TODO: why does this not exit?
+    
+    if len(sys.argv) > 2:
+        logfile = sys.argv[2]
+    
+    if len(sys.argv) > 3:
+        db = sys.argv[3]
+    
+    if len(sys.argv) > 4:
+        mails = sys.argv[4].split(',')
 
     #try:
     #    with PyCallGraph(output=GraphvizOutput()):
     #        print " *** Running in pycallgraph *** "
-    #        main(db, rules, logfile)
+    #        main(db, rules, logfile, mails)
     #except: # TODO: only catch pycallgraph errors
-    #    main(db, rules, logfile)
+    #    main(db, rules, logfile, mails)
     
-    main(db, rules, logfile)
+    main(db, rules, logfile, mails)
 
 else:
     print "Non-recognized caller: "+__name__
