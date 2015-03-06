ad9465bfb31ce6acd48f096064a5189ff15a3342
Martin Hvidberg <Martin@Hvidberg.net>
2015-03-06 19:11:50 +0100

    Update data_checker.py
    uploading from production environment '150306/mahvi

--- data_checker.py
+++ data_checker.py
@@ -2,25 +2,33 @@ import arcpy
 import os
 
 import utils
+import S57names
 
 TEST_RULES_ONLY = True
 
-def SetUpDB(db):
+def SetUpDB(db, withEditSess):
     """Connect to database with .sde connection, and prepare a version for editing."""
 
-    print "   < SetUpDB() >"
+    utils.log("   < SetUpDB() >")
     arcpy.env.workspace = db # does this do anything?
+    utils.log("   "+db)
     
     # Determine connection type - connType = 0 for .gdb, 1 for .sde
     connType = 0
     descDB = arcpy.Describe(db)
-    if descDB.workspaceFactoryProgID == "esriDataSourcesGDB.SdeWorkspaceFactory.1":
+    
+    if descDB.workspaceFactoryProgID == "esriDataSourcesGDB.SdeWorkspaceFactory.1" or descDB.workspaceFactoryProgID == "esriDataSourcesGDB.SdeWorkspaceFactory":
         connType = 1
-        print "     It's an SDE geodatabase"
+        utils.log("     It's an SDE geodatabase")
         pass
     elif descDB.workspaceFactoryProgID != "esriDataSourcesGDB.FileGDBWorkspaceFactory.1":
-        print "Unexpected data format. Should be .sde or .gdb."
+        utils.log("Unexpected data format. Should be .sde or .gdb, was {}".format(descDB.workspaceFactoryProgID))
         return
+    #connType = arcpy_utils.GetConnectionType(descDB)
+    #if connType == -1:
+    #    utils.log("Unexpected data format. Should be .sde or .gdb, was {}".format(descDB.workspaceFactoryProgID))
+    #    return
+        
     
     # We don't need to use an edit version, changes will still be recognised when the products do Get Changes.
     # NIS_MODIFIED etc. will be left unchanged
@@ -35,7 +43,8 @@ def SetUpDB(db):
 #    arcpy.ChangeVersion_management("Nautical/CulturalFeaturesL", "TRANSACTIONAL", ""
     
     # we don't need Editor, startEditing and startOperation for file .gdbs
-    if connType == 1:
+    # or if there are no FIX rules
+    if connType == 1 and withEditSess:
         # Start an edit session. Must provide the workspace.
         #  http://resources.arcgis.com/en/help/main/10.2/index.html#//018w00000005000000
         edit = arcpy.da.Editor(db)
@@ -46,7 +55,6 @@ def SetUpDB(db):
         
         # Start an edit operation
         edit.startOperation()
-        
     else:
         edit = None
     
@@ -57,7 +65,7 @@ def SetUpDB(db):
         db += "/NIS.Nautical/NIS."
     else:
         db += "/Nautical/"
-#     print "db = {}".format(db)
+#     utils.log("db = {}".format(db))
     
     return [db, edit]
 
@@ -65,20 +73,22 @@ def SetUpDB(db):
 def FinishUpDB(db, edit):
     """Reconcile and Post the version, and clean up."""
     
-    print "   < FinishUpDB() >"
-    # Reconcile and Post the version, if allowed by the run mode
-    # TODO?
-    # arcpy.ReconcileVersions_management
-    
-    # Stop the edit operation
-    edit.stopOperation()
-    
-    # Stop the edit session and save the changes
-    edit.stopEditing(True)
-   
-    # Remove the version if posted
-    # TODO?
-    # arcpy.DeleteVersion_management(db, newName)
+    utils.log("   < FinishUpDB() >")
+
+    if edit is not None: # is there an active edit session
+        # Reconcile and Post the version, if allowed by the run mode
+        # TODO?
+        # arcpy.ReconcileVersions_management
+        
+        # Stop the edit operation
+        edit.stopOperation()
+        
+        # Stop the edit session and save the changes
+        edit.stopEditing(True)
+       
+        # Remove the version if posted
+        # TODO?
+        # arcpy.DeleteVersion_management(db, newName)
     
     # Disconnect from db
     # TODO???
@@ -92,13 +102,18 @@ def CheckData(dataset, rules):
     # db_str is something like "Database Connections/yellow_nis@NIS_EDITOR.sde/NIS.Nautical/NIS."
     # or "file.gdb/Nautical/" - which can be suffixed with a fc name like "CulturalFeaturesA"
 
-    print "   < CheckData() >"
+    utils.log("   < CheckData() >")
+    
+    hasEdits = False
+    for rule in rules:
+        if rule.dofix:
+            hasEdits = True
     
     # Set up database connection and edit session
     try:
-        db_str, db_editSess = SetUpDB(dataset)
-    except:
-        print "Problem connecting to database, quitting"
+        db_str, db_editSess = SetUpDB(dataset, hasEdits)
+    except Exception as exc:
+        utils.log("Problem connecting to database, quitting.\n"+str(exc))
         return 102
     
     # *** Walk the db, and use the rules on it - This is the big enchilada
@@ -117,13 +132,13 @@ def CheckTables(dataset, rules):
     # If you get: "RuntimeError: An expected Field was not found or could not be retrieved properly."
     # don't use double quotes in condition input, use single quotes!!!
 
-    print "   < CheckTables() >"
+    utils.log("   < CheckTables() >")
     totalFixes = 0
     totalLogs = 0
     
     for rule in rules:
         print rule.id;
-        if(rule.dofix): # FIX
+        if(rule.dofix): # FIX (no LOVE rules here)
             # v. 0.1:
             # print "UPDATE " + str(rule.fclist) + " SET " + rule.field + " = " + rule.fixvalue + " WHERE " + rule.GetWhereString()
             for fc in rule.fclist:
@@ -155,17 +170,35 @@ def CheckTables(dataset, rules):
             for fc in rule.fclist:
                 count = 0
                 fields = ["OBJECTID", "LNAM"] + rule.fixLst # fields to get from the db, update log writing and reportValues below if this changes
-                with arcpy.da.SearchCursor(dataset+fc, fields, where_clause=rule.GetWhereString()) as sc:
-                    for row in sc:
-                        count += 1
-                        reportValues = ''
-                        if(rule.fixLst):
-                            for i,field in enumerate(rule.fixLst):
-                                reportValues += ', '+field+"="+utils.encodeIfUnicode(row[i+2])
-                            reportValues = ' (' + reportValues[2:] + ')'
-                        utils.log("Found rule {} ({}) violation for OBJECTID={} in {}{}"
-                            .format(rule.id, rule.title, row[0], fc, reportValues))
-                utils.log("Total {} search hits for rule {} ({})".format(count, rule.id, fc))
+                if (rule.mode == 'LOVE'):
+                    fields.append(rule.condition[0]) # include the field we're testing
+                try:
+                    with arcpy.da.SearchCursor(dataset+fc, fields, where_clause=rule.GetWhereString()) as sc:
+                        for row in sc:
+                            reportValues = ''
+                            if rule.fixLst:
+                                for i,field in enumerate(rule.fixLst):
+                                    reportValues += ', '+field+"="+utils.encodeIfUnicode(row[i+2])
+                                    if field == 'FCSUBTYPE':
+                                        reportValues += '/{}'.format(S57names.FCFCS2ABB(fc, str(row[i+2])))
+                                reportValues = ' (' + reportValues[2:] + ')'
+                            if(rule.mode == 'SQL'):
+                                count += 1
+                                utils.log("Found SQL rule {} violation ({}) for OBJECTID={} in {}{}"
+                                    .format(rule.id, rule.title, row[0], fc, reportValues))
+                            elif rule.mode == 'LOVE' :
+                                for val in row[-1].split(','):
+                                    if not val in rule.condition[1]:
+                                        count += 1
+                                        utils.log("Found LOVE rule {} violation ({}) for OBJECTID={} in {} ({}: {} in {}){}"
+                                            .format(rule.id, rule.title, row[0], fc, rule.condition[0], val, row[-1], reportValues))
+                                        break
+                except Exception as err:
+                    utils.log("Found error in rule {}, check SQL syntax (e.g. unknown fieldname or string field treated as number): {}.{}, {}".format(rule.id, fc, rule.fcsubtype, rule.GetWhereString()))
+                    print err
+                if count > 0:
+                    utils.log("Total {} search hits for rule {} ({}: {}; WHERE {})".format(count, rule.id, fc, rule.title, rule.GetWhereString()))
                 totalLogs += count
-    utils.log("Done checking tables, total {} log hits and {} fixes.".format(totalLogs, totalFixes))
+    utils.log("     Done checking tables, total {} log hits and {} fixes.".format(totalLogs, totalFixes))
+    print "Done checking tables, total {} log hits and {} fixes.".format(totalLogs, totalFixes)
 
